2b0e55acc7572d94b49dcd7e224af91df0588b09
Artur Białoskórski <bialoskorski.artur@gmail.com>
2019-07-05 23:10:20 +0200

    uruk-hai made todo's appear and blowed up the dam

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
+import Todos from "./Todos";
 
-function App() {
-	return <div className="App" />;
+class App extends Component {
+	state = {
+		todos: [
+			{ id: 1, content: "visit the Helm's Deep" },
+			{ id: 2, content: "Call for help from Gondor" },
+		],
+	};
+	render() {
+		return (
+			<div className="App">
+				<h1 className="center blue-text">List of Todo's</h1>
+				<Todos todos={this.state.todos} />
+			</div>
+		);
+	}
 }
-
 export default App;
